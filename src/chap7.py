cc6242db741b22d0ae453dc87119bf97968a1e0d
junkata <junkata.dev@gmail.com>
2023-03-19 16:26:33 +0900

    add chap7

---
+++ src/chap7.py
@@ -0,0 +1,102 @@
+import pandas as pd
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('./sukkiri-ml-codes/datafiles/Survived.csv')
+print(df.head(2))
+print(df.isnull().sum())
+
+df['Age'] = df['Age'].fillna(df['Age'].mean())
+
+# 決定木では多少の外れ値はそれほど影響をうけない
+
+col = ['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
+x = df[col]
+t = df['Survived']
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, t, test_size=0.2, random_state=0)
+print(x_train.shape)
+
+# model
+# 不均衡データの場合はclass_weight='balanced'を設定
+model = tree.DecisionTreeClassifier(
+    max_depth=5,
+    random_state=0,
+    class_weight='balanced')
+model.fit(x_train, y_train)
+print(model.score(X=x_test, y=y_test))
+
+# max_depthが深いと過学習が起こる。
+# 過学習が起こると特に重要ではない条件が判定されてしまうのでscoreが下がる
+# 過学習を回避するためには
+# - データ数を増やす
+# - データの前処理方法を変える
+# - モデルの学習時の設定を変える
+# - モデル自体を変える
+
+# 前処理を変える
+df2 = pd.read_csv('./sukkiri-ml-codes/datafiles/Survived.csv')
+
+print(df2.groupby('Pclass').mean()['Age'])
+
+# ピボットテーブル(クロス集計)はデフォルトで平均を返す
+ptable = pd.pivot_table(df2, index='Survived', columns='Pclass', values='Age')
+print(ptable)
+
+# 最大値
+ptable = pd.pivot_table(
+    df2,
+    index='Survived',
+    columns='Pclass',
+    values='Age',
+    aggfunc='max')
+print(ptable)
+
+# 穴埋めを各グループ化して行う
+is_null = df2['Age'].isnull()
+
+# pivotで計算された pclas=1 survived=0 のAge平均を代入
+df2.loc[(df2['Pclass'] == 1) & (
+    df2['Survived'] == 0) & (is_null), 'Age'] = 43
+# p=1, s=1
+df2.loc[(df2['Pclass'] == 1) & (
+    df2['Survived'] == 1) & (is_null), 'Age'] = 35
+
+# p=2, s=0
+df2.loc[(df2['Pclass'] == 2) & (
+    df2['Survived'] == 0) & (is_null), 'Age'] = 33
+# p=2, s=1
+df2.loc[(df2['Pclass'] == 2) & (
+    df2['Survived'] == 1) & (is_null), 'Age'] = 25
+
+# p=3, s=0
+df2.loc[(df2['Pclass'] == 3) & (
+    df2['Survived'] == 0) & (is_null), 'Age'] = 26
+# p=3, s=1
+df2.loc[(df2['Pclass'] == 3) & (
+    df2['Survived'] == 1) & (is_null), 'Age'] = 20
+
+col = ['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
+x = df2[col]
+t = df2['Survived']
+
+
+def learn(x, t, depth=3):
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, t, test_size=0.2, random_state=0)
+    model = tree.DecisionTreeClassifier(
+        max_depth=depth,
+        random_state=0,
+        class_weight='balanced')
+
+    model.fit(x_train, y_train)
+    score = model.score(X=x_train, y=y_train)
+    score2 = model.score(X=x_test, y=y_test)
+    return round(score, 3), round(score2, 3), model
+
+
+for j in range(1, 15):
+    s1, s2, m = learn(x, t, depth=j)
+    sentence = '深さ{}: 訓練データの精度{}: テストデータの精度{}'
+    print(sentence.format(j, s1, s2))
